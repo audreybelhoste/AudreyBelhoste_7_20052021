1f155b70f5092f0f50158646281e35787bbc05bd
Audrey Belhoste <audrey.belhoste@gmail.com>
2021-05-28 21:42:12 +0200

    tag ingredient -wip

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,10 @@
 const list = document.querySelector('#recipes');
+let allIngredients = [];
 
 createDOM();
 
 function createDOM(){
+
 	for(let i = 0; i < recipes.length; i++){
 		const cardContainer = document.createElement('div');
 		const card = document.createElement('div');
@@ -16,37 +18,28 @@ function createDOM(){
 		const descriptionContainer = document.createElement('div');
 		const description = document.createElement('p');
 	
-		cardContainer.classList.add('col-md-12');
-		cardContainer.classList.add('col-lg-6');
-		cardContainer.classList.add('col-xl-4');
-		cardContainer.classList.add('blocRecipe');
-		card.classList.add('card');
-		card.classList.add('mb-4');
+		cardContainer.classList.add('col-md-12', 'col-lg-6', 'col-xl-4', 'blocRecipe');
+		card.classList.add('card', 'border-0', 'mb-4');
 		img.classList.add('card-img-top');
 		cardBody.classList.add('bg-secondary');
-		infos.classList.add('row');
-		infos.classList.add('justify-content-between');
-		title.classList.add('col-7');
-		time.classList.add('col-4');
-		time.classList.add('text-right');
-		time.classList.add('font-weight-bold');
-		time.classList.add('h2');
-		time.classList.add('time');
-		content.classList.add('row');
-		content.classList.add('mt-2');
+		infos.classList.add('row', 'justify-content-between');
+		title.classList.add('col-9');
+		time.classList.add('col-3', 'text-right', 'font-weight-bold', 'h2', 'time', 'position-relative');
+		content.classList.add('row', 'mt-2');
 		ingredientsList.classList.add('col');
 		descriptionContainer.classList.add('col');
 		description.classList.add('description');
 	
-		cardContainer.setAttribute('idRecipe', recipes[i].name);
+		cardContainer.setAttribute('nameRecipe', recipes[i].name);
 		img.src = 'img.svg';
 		cardBody.classList.add('card-body');
+		cardBody.classList.add('rounded-bottom');
 		title.textContent = recipes[i].name;
 		time.textContent = recipes[i].time + ' min';
 		description.textContent = recipes[i].description;
 	
 		const ingredients = recipes[i].ingredients;
-	
+		
 		for(let j = 0; j < ingredients.length; j++){
 	
 			const ingredient = document.createElement('p');
@@ -64,8 +57,12 @@ function createDOM(){
 			}
 	
 			ingredientsList.appendChild(ingredient);
+
+			if(!allIngredients.includes(ingredients[j].ingredient)){
+				allIngredients.push(ingredients[j].ingredient);
+			}
 		}
-	
+
 		list.appendChild(cardContainer);
 		cardContainer.appendChild(card);
 		card.appendChild(img);
@@ -77,5 +74,20 @@ function createDOM(){
 		content.appendChild(ingredientsList);
 		content.appendChild(descriptionContainer);
 		descriptionContainer.appendChild(description);
+	}
+
+	displayIngredients(allIngredients);
+}
+
+function displayIngredients(allIngredients){
+	const ingredientList = document.querySelector('.ingredientList');
+	for(let i = 0; i < allIngredients.length; i++){
+		const ingredientItem = document.createElement('li');
+		const ingredientLink = document.createElement('a');
+		ingredientLink.href = '#';
+		ingredientLink.classList.add('dropdown-item', 'ingredientItem');
+		ingredientLink.textContent = allIngredients[i];
+		ingredientList.appendChild(ingredientItem);
+		ingredientItem.appendChild(ingredientLink);
 	}
 }
--- js/search.js
+++ js/search.js
@@ -1,41 +1,165 @@
 const searchBar = document.getElementById('searchBar');
 const blocsRecipe = document.querySelectorAll('.blocRecipe');
+const ingredientItem = document.querySelectorAll('.ingredientItem');
+const dropdownToggle = document.querySelectorAll('.dropdown-toggle');
+const searchIngredient = document.getElementById('searchIngredient');
 
-searchBar.addEventListener('keyup', function(element){
-	searchTerm = searchBar.value.toLowerCase();
-	searchInData(searchTerm);
+let recipesToDisplay = [];
+recipesToDisplay = recipes;
+// search bar
+let searchTerms = [];
+// ingredient tags
+searchTerms[1] = [];
+
+searchBar.addEventListener('keyup', function(){
+	searchTerm = searchBar.value.toLowerCase(); 
+	searchTerms[0] = searchTerm;
+
+	if(searchTerm.length > 2){
+		searchInData(searchTerms);
+	} 
+	else {
+		blocsRecipe.forEach(element => {
+			noResult.classList.add('invisible');
+			element.classList.remove('d-none');
+			searchInData(searchTerms);
+		});
+	}
 })
 
-function searchInData(searchTerm){
-	let recipesToDisplay = new Array();
-	recipes.map(function(recipe){
-		if(recipe.name.toLowerCase().includes(searchTerm) || recipe.description.toLowerCase().includes(searchTerm)){
-			recipesToDisplay.push(recipe.name);
-		}
+dropdownToggle.forEach(function(element) {
+	element.addEventListener('click', function() {
+		this.querySelector('.dropdownTitle').classList.add('d-none');
+		this.querySelector('.searchTag').classList.remove('d-none');
+	})
+})
 
-		for(let i = 0; i < recipe.ingredients.length; i++){
-			if(recipe.ingredients[i].ingredient.includes(searchTerm) && !recipesToDisplay.indexOf(recipe.id)){
-				recipesToDisplay.push(recipe.name);
+searchIngredient.addEventListener('keyup', function(){
+	ingredientItem.forEach(element => {
+		element.classList.remove('d-none');
+	});
+
+	for(let i = 0; i < ingredientItem.length; i++){
+		if(!ingredientItem[i].textContent.toLowerCase().includes(searchIngredient.value.toLowerCase())){
+			ingredientItem[i].classList.add('d-none');
+		}
+	}
+})
+
+ingredientItem.forEach(function(element) {
+	element.addEventListener('click', function() {
+		searchTerms[1].push(element.textContent);
+		searchInData(searchTerms);
+	})
+})
+
+function searchInData(searchTerms){
+	recipesToDisplay = recipes.filter(function(recipe){
+		
+		if(searchTerms[0]){
+			if(!recipeSearch(recipe, searchTerms)){
+				return false;
 			}
 		}
+
+		if(searchTerms[1].length > 0){
+			
+			console.log(ingredientsSearch(recipe, searchTerms))
+
+			if(!ingredientsSearch(recipe, searchTerms)){
+				
+				return false;
+			}
+		}
+
+		
+
+		return recipesToDisplay;
 	});
 
 	displayRecipes(recipesToDisplay);
+	displayIngredients(recipesToDisplay);
 };
 
-function clearDOMGallery(){
-	var recipeSection = document.querySelector('#recipes');
-	recipeSection.innerHTML = '';
+function recipeSearch(recipe, searchTerms){
+	if(recipe.name.toLowerCase().includes(searchTerms[0]) || recipe.description.toLowerCase().includes(searchTerms[0])){
+		return true;
+	} else {
+		for(let i = 0; i < recipe.ingredients.length; i++){
+			if(recipe.ingredients[i].ingredient.includes(searchTerms[0])){
+				return true;
+			}
+		}
+	}
+}
+
+function ingredientsSearch(recipe, searchTerms){
+	let globResult = true;
+
+	searchTerms[1].forEach(function(searchTerm){
+		let result = false;
+
+		for(let i = 0; i < recipe.ingredients.length; i++){
+			if(recipe.ingredients[i].ingredient.includes(searchTerm)){
+				result = true;
+			}
+		}
+
+		console.log(result)
+
+		if(!result){
+			globResult = false;
+		}
+
+	})
+
+	return globResult;
 }
 
 function displayRecipes(recipesToDisplay){
+	if(recipesToDisplay.length == 0){
+		noResult.classList.remove('invisible');
+	} else {
+		noResult.classList.add('invisible');
+	}
+		
 	blocsRecipe.forEach(element => {
 		element.classList.remove('d-none');
 	});
+
+	let nameRecipes = [];
+
+	recipesToDisplay.forEach(function(recipe){
+		nameRecipes.push(recipe.name)
+	})
+
 	for(let i = 0; i < blocsRecipe.length; i++){
-		let idRecipe = blocsRecipe[i].getAttribute('idRecipe');
-		if(!recipesToDisplay.includes(idRecipe)){
+		let nameRecipe = blocsRecipe[i].getAttribute('nameRecipe');
+		if(!nameRecipes.includes(nameRecipe)){
 			blocsRecipe[i].classList.add('d-none');
 		}
 	}
+}
+
+function displayIngredients(recipesToDisplay){
+	ingredientItem.forEach(element => {
+		element.classList.remove('d-none');
+	});
+
+	let ingredients = [];
+		for(let i = 0; i < recipesToDisplay.length; i++){
+			for(let j = 0; j < recipesToDisplay[i].ingredients.length ; j++){
+				recipesToDisplay[i].ingredients.forEach(function(ingredient){
+					if(!ingredients.includes(ingredient.ingredient)){
+						ingredients.push(ingredient.ingredient);
+					}
+				})
+			}
+		}
+	
+	ingredientItem.forEach(function(ingredient){
+		if(!ingredients.includes(ingredient.textContent)){
+			ingredient.classList.add('d-none');
+		}
+	})
 }
